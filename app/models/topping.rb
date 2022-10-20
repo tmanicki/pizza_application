d19c22ddbd9babad3a74f97779f767bad78a2368
Tim Manicki <tmanicki@asu.edu>
2022-10-19 22:19:27 -0700

    Validation: prevents blank, prevents duplicates, and requires char only!

--- app/models/topping.rb
+++ app/models/topping.rb
@@ -1,2 +1,3 @@
 class Topping < ApplicationRecord
+    validates :name, uniqueness: { case_sensitive: false, message: "already exists" }, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "must contain letters only" }
 end
