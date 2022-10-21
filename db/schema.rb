b8dca963b05b148ef6554fe5a8144749c5a1d113
Tim Manicki <tmanicki@asu.edu>
2022-10-20 22:40:40 -0700

    added title to Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_010221) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_053949) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_010221) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "title"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
