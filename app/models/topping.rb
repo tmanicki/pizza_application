e1646023cc4fc2f88884453b4aabc20d98b78334
Tim Manicki <tmanicki@asu.edu>
2022-10-21 12:09:38 -0700

    Added space as allowed in regexp

--- app/models/topping.rb
+++ app/models/topping.rb
@@ -1,3 +1,3 @@
 class Topping < ApplicationRecord
-    validates :name, uniqueness: { case_sensitive: false, message: "Topping already exists" }, presence: {message: "Name field must be filled"}, format: { with: /\A[a-zA-Z]+\z/, message: "Topping must contain letters only" }
+    validates :name, uniqueness: { case_sensitive: false, message: "Topping already exists" }, presence: {message: "Name field must be filled"}, format: { with: /\A[a-zA-Z\s]+\z/, message: "Topping must contain letters only" }
 end
