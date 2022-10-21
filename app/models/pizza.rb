2a0a444d167d0bf50c339f9aa529f4d4d2748ca5
Tim Manicki <tmanicki@asu.edu>
2022-10-21 16:30:48 -0700

    Get Guis to match

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,3 +1,4 @@
 class Pizza < ApplicationRecord
     has_and_belongs_to_many :toppings
+    validates :pName, uniqueness: { case_sensitive: false, message: "Topping already exists" }, presence: {message: "Name field must be filled"}, format: { with: /\A[a-zA-Z\s]+\z/, message: "Topping must contain letters only" }
 end
